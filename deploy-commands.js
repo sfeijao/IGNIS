d92ffa5ed5bf0d8e091fdcd8a58c294a81221ccb
Seu Nome <seu@email.com>
2025-09-08 18:52:30 +0000

    registro de comandos em todos os servers

---
+++ deploy-commands.js
@@ -0,0 +1,37 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+// Carregar config
+const config = require('./config.json');
+const token = config.DISCORD.TOKEN;
+const clientId = config.DISCORD.CLIENT_ID;
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+    }
+}
+
+const rest = new REST().setToken(token);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands...');
+
+        await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
